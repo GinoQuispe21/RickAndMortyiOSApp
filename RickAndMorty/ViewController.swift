6d9c35db8c75b5c15e24452f8c8825d70e934d7b
Gino Salvador Quispe Calixto <gquispec@proveedor.intercorp.com.pe>
2023-08-20 12:36:32 -0500

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Gino Salvador Quispe Calixto on 20/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
