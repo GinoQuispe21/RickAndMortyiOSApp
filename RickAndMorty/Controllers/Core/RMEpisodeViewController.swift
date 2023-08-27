e0dd45bb149ed0cd513f2b11028eb46ccd4449fa
Gino Salvador Quispe Calixto <gquispec@proveedor.intercorp.com.pe>
2023-08-27 16:19:11 -0500

    Created rick and morty project

---
+++ RickAndMorty/Controllers/Core/RMEpisodeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RMEpisodeViewController.swift
+//  RickAndMorty
+//
+//  Created by Gino Salvador Quispe Calixto on 23/08/23.
+//
+
+import UIKit
+
+class RMEpisodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ RickAndMorty/Models/RMEpisode.swift
@@ -0,0 +1,8 @@
+//
+//  RMEpisode.swift
+//  RickAndMorty
+//
+//  Created by Gino Salvador Quispe Calixto on 23/08/23.
+//
+
+import Foundation
